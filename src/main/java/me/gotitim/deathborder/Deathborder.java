5fec7c2131ef7c2762bd0599f509b5bf55af2118
goteusz-maszyk <mateusz.goszyk@gmail.com>
2022-07-26 18:45:07 +0200

    Effects are now applied

--- src/main/java/me/gotitim/deathborder/Deathborder.java
+++ src/main/java/me/gotitim/deathborder/Deathborder.java
@@ -1,17 +1,68 @@
 package me.gotitim.deathborder;
 
+import me.gotitim.deathborder.listener.PlayerListener;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public final class Deathborder extends JavaPlugin {
+    public static final Map<UUID, Integer> playerEffects = new HashMap<>();
+    private static Deathborder instance;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        instance = this;
 
+        getServer().getPluginManager().registerEvents(new PlayerListener(), this);
+
+        saveDefaultConfig();
+
+        getServer().getOnlinePlayers().forEach((Player player) -> {
+            playerEffects.put(player.getUniqueId(), -1);
+        });
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public boolean isPlayerInBorder(Player player) {
+        ConfigurationSection worldBorderData = getBorder(player);
+
+        if (worldBorderData == null) return true;
+        Location location = player.getLocation();
+
+        if(worldBorderData.getInt("x", (int) Math.abs(location.getX())) <= Math.abs(location.getX())) return false;
+        if(worldBorderData.getInt("z", (int) Math.abs(location.getZ())) <= Math.abs(location.getZ())) return false;
+        return true;
+    }
+
+    @Nullable
+    public ConfigurationSection getBorder(Player player) {
+        return getBorder(player.getWorld());
+    }
+
+    @Nullable
+    public ConfigurationSection getBorder(World world) {
+        ConfigurationSection borders = getConfig().getConfigurationSection("borders");
+        if(borders == null) {
+            Bukkit.getLogger().warning("Warning: No borders specified in config.yml!");
+            return null;
+        }
+
+        return borders.getConfigurationSection(world.getName());
+    }
+
+    public static Deathborder getInstance() {
+        return instance;
     }
 }
--- src/main/java/me/gotitim/deathborder/listener/PlayerListener.java
+++ src/main/java/me/gotitim/deathborder/listener/PlayerListener.java
@@ -1,33 +1,83 @@
 package me.gotitim.deathborder.listener;
 
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
 import static me.gotitim.deathborder.Deathborder.getInstance;
+import static me.gotitim.deathborder.Deathborder.playerEffects;
 
 public class PlayerListener implements Listener {
-    private static final Map<UUID, Integer> playerEffectLevels = new HashMap<>();
+    private static final Map<UUID, Integer> effectTasks = new HashMap<>();
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        playerEffects.put(event.getPlayer().getUniqueId(), -1);
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Bukkit.getScheduler().cancelTask(effectTasks.get(event.getPlayer().getUniqueId()));
+    }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
 
-        if(!getInstance().isPlayerInBorder(player)) return;
+        ConfigurationSection border = getInstance().getBorder(player);
+        if(border == null) return;
 
-        player.sendTitle("You exited safe world area!", "Be careful.", 5, 40, 5);
+        if(getInstance().isPlayerInBorder(player)) {
+            playerEffects.put(player.getUniqueId(), -1);
+            if(!effectTasks.containsKey(player.getUniqueId())) return;
+            int taskID = effectTasks.get(player.getUniqueId());
+            effectTasks.remove(player.getUniqueId());
+            Bukkit.getScheduler().cancelTask(taskID);
+            return;
+        }
 
-        String[] effectsToApply = getInstance().getBorder(player).getStringList("effects").toArray(new String[0]);
-        Integer effectsInterval = getInstance().getBorder(player).getInt("effectsInterval");
+        if(playerEffects.get(player.getUniqueId()) == -1) {
+            player.sendTitle("You exited safe world area!", "Be careful.", 5, 40, 5);
+        }
 
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(getInstance(), () -> {
+        if(effectTasks.containsKey(player.getUniqueId())) return;
+        playerEffects.put(player.getUniqueId(), 0);
 
-        }, effectsInterval*20, effectsInterval*20);
+        String[] effectsSequence = border.getStringList("effects").toArray(new String[0]);
+        int effectsInterval = border.getInt("effectsInterval");
+
+        effectTasks.put(player.getUniqueId(), Bukkit.getScheduler().scheduleSyncRepeatingTask(getInstance(), () -> {
+            int sequenceLevel = playerEffects.get(player.getUniqueId());
+            String[] effectsToApply = effectsSequence[sequenceLevel].split("-");
+            Bukkit.getLogger().info(String.valueOf(sequenceLevel));
+            for (String effectData : effectsToApply) {
+                Bukkit.getLogger().info(effectData);
+                if(effectData.equalsIgnoreCase("KILL")) {
+                    player.setHealth(0);
+                } else {
+                    String[] effect = effectData.split("_");
+                    try {
+                        PotionEffectType type = PotionEffectType.getByName(effect[0]);
+                        player.addPotionEffect(new PotionEffect(Objects.requireNonNull(type), Integer.parseInt(effect[2]) * 20, Integer.parseInt(effect[1]) - 1, false, true, true));
+                    } catch (Exception e) {
+                        Bukkit.getLogger().warning("Can't find effect with name " + effect[0] + ". See https://hub.spigotmc.org/javadocs/bukkit/org/bukkit/potion/PotionEffectType.html ");
+                    }
+                }
+            }
+
+            playerEffects.put(player.getUniqueId(), sequenceLevel+1);
+        }, 0, effectsInterval * 20L));
     }
 }
